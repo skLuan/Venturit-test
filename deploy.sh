f1ff57073059288c745796d52d9d352cb2274239
Luan E <luan.erazo@skyrocketlab.space>
2021-02-05 09:36:24 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env sh
 
 # abort on errors
-set -e
+# set -e
 
 # build
 npm run build
 
+git init
+git add -A
+git commit -m 'deploy'
+
 # navigate into the build output directory
 cd dist
 
-git push -f git@github.com:skLuan/Venturit-test.git master:gh-pages
+git push -f git@github.com:skLuan/Venturit-test.git main:gh-pages
 
 cd -
