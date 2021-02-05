984ba2855d8a52fa4ab826281f1095c052a0ec24
Luan E <luan.erazo@skyrocketlab.space>
2021-02-05 09:55:38 -0500

    dist carpet

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,9 @@
 # build
 npm run build
 
-git init
-git add -A
-git commit -m 'deploy'
-
 # navigate into the build output directory
 cd dist
 
 git push -f git@github.com:skLuan/Venturit-test.git main:gh-pages
 
-cd -
+# cd -
